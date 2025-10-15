a0052590865422caeada9df1af309b43e922493f
ofeliapnr <ofeliapnr@gmail.com>
2025-10-15 01:19:05 +0000

    added functiomns to script

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 //events blah blah
-alert('test')
+// https://www.youtube.com/watch?v=gHMBdpTx-Ys (tutorial I used)
+
+//get button & dropdown content elements
+const dropdownButton = document.getElementById('dropdownButton');
+const dropdownContent = document.getElementById('dropdownContent');
+
+//listen for clicks on the dropdown button
+dropdownButton.addEventListener('click', ()=>{
+    //toggle visibility of dropdown content
+    dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
+});
+
+window.addEventListener('click', (event) => {
+    //close dropdown if click is outside
+    dropdownContent.style.display = 'none';
+});
